4dfe80a5ffa9a69694a2dc463e7421970abd3ff4
Lydia Wagner <lydiawagner@gmail.com>
2016-07-21 10:49:23 -0400

    Refactoring lightbox opening and handling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-//
-// Task Backlog
-//
-// Wide screen adaptive display
-// Lazy load for thumbs
-// Deep linking/history.pushState
-// 
-
 var lightbox = {
 
   flickrApiKey: "f1a836ca91876fec109588c72ecc641d",
@@ -13,7 +5,9 @@ var lightbox = {
   photoTotal: 0,
 
   init: function() {
-    window.addEventListener('DOMContentLoaded', lightbox.getPhotosFromAPI, false);
+
+    window.addEventListener("DOMContentLoaded", lightbox.getPhotosFromAPI, false);
+
   },
 
   // construct args for photo-getting API call
@@ -26,7 +20,7 @@ var lightbox = {
       "arg": "user_id=" + lightbox.flickrUserID
     };
 
-    photoData = lightbox.callFlickrAPI(flickrAPIArgs, function() {
+    lightbox.callFlickrAPI(flickrAPIArgs, function() {
       photoData = JSON.parse(this.responseText);
       lightbox.parseJSON(photoData.photos.photo);
     });
@@ -38,7 +32,6 @@ var lightbox = {
 
     var flickrAPICall, xhr;
 
-    // using template literals would be pretty sweet here, WOULDN'T IT IE 11
     flickrAPICall = "https://api.flickr.com/services/rest/?method=" + elem.method;
     flickrAPICall += "&extras=original_format&api_key=" + lightbox.flickrApiKey + "&";
     flickrAPICall += elem.arg + "&format=json&nojsoncallback=?";
@@ -49,8 +42,7 @@ var lightbox = {
       if (xhr.readyState === 4 && xhr.status === 200) {
         callback.apply(xhr);
       } else {
-        //  console.log(xhr.status);
-        //  console.log(xhr.readyState);
+        //console.log("xhr.status: " + xhr.status + " & xhr.readyState: " + xhr.readyState);
       }
     };
     xhr.send();
@@ -60,62 +52,45 @@ var lightbox = {
   // assembling photo URLs from the data response
   parseJSON: function(data) {
 
-    var key, imgObj, farmID, serverID, originalSecretID, photoID, photoTitle, imgSrc, thumbPhotoSrc, detailPhotoSrc, detailBigPhotoSrc;
+    var key, imgObj, imgSrc, thumbPhotoSrc, detailPhotoSrc, detailBigPhotoSrc;
     var thumbHTML = '';
-    var imgObjArray = [];
 
     for (key in data) {
       if (data.hasOwnProperty(key)) {
 
-        // Flickr's recommended action to create photo source URLs
-        // In this case, to use small images for thumbs and larger for the lightbox
-        // https://www.flickr.com/services/api/misc.urls.html
+        // Flickr's recommended action to create photo source URLs https://www.flickr.com/services/api/misc.urls.html
         imgObj = data[key];
-        farmID = imgObj.farm;
-        serverID = imgObj.server;
-        secretID = imgObj.secret;
-        originalSecretID = imgObj.originalsecret;
-        photoID = imgObj.id;
-        photoTitle = imgObj.title;
 
-        imgObjArray.push(photoID);
+        imgSrc = "https://farm" + imgObj.farm + ".staticflickr.com/" + imgObj.server + "/" + imgObj.id + "_";
 
-        imgSrc = "https://farm" + farmID + ".staticflickr.com/" + serverID + "/" + photoID + "_";
-
-        thumbPhotoSrc = imgSrc + secretID + "_q.jpg";
-        detailPhotoSrc = imgSrc + secretID + "_b.jpg";
+        thumbPhotoSrc = imgSrc + imgObj.secret + "_q.jpg";
+        detailPhotoSrc = imgSrc + imgObj.secret + "_b.jpg";
 
         // in case one feels ambitious and builds screen-adaptive photo loading
-        detailBigPhotoSrc = imgSrc + originalSecretID + "_o.jpg";
+        detailBigPhotoSrc = imgSrc + imgObj.originalsecret + "_o.jpg";
 
-        thumbHTML += "<li class='img' title='" + photoTitle + "' id='" + photoID + "' data-pid='" + key + "'";
+        thumbHTML += "<li class='img' title='" + imgObj.title + "' id='" + imgObj.id + "' data-pid='" + key + "'";
         thumbHTML += " data-img='" + detailPhotoSrc + "' style='background-image:url(" + thumbPhotoSrc + ")'></li>";
       }
     }
 
-    // get the total number of photos so we know when next and previous reset
     lightbox.photoTotal = Number(key);
-
-    // situate the HTML and captions
     lightbox.placeHTML(thumbHTML);
 
   },
 
-  // set photo thumbs into container and initiate click and key handlers and thumb container placement
+  // set photo thumbs into container and set event listeners for clicking or keypressing
   placeHTML: function(elem) {
 
     document.getElementById("container").innerHTML = elem;
 
-    // set event listeners for clicking or keypressing
     document.getElementById("container").addEventListener("click", lightbox.openLightbox, false);
     document.getElementById("lightbox").addEventListener("click", lightbox.advanceLightbox, false);
     window.addEventListener("keydown", lightbox.keyManipulateLightbox, false);
 
-    // set width of container element for nicer display
-    lightbox.sizeContainer();
-
   },
 
+  // add visibility class, add hint, call photo
   openLightbox: function(elem) {
 
     document.getElementById("lightbox").classList.add('open');
@@ -124,41 +99,40 @@ var lightbox = {
 
   },
 
-  // keypress handlers 
+    // remove class that makes lightbox visible
+  closeLightbox: function() {
+
+    document.getElementById("lightbox").classList.remove('open');
+
+  },
+
+  // hide hint!
+  hideHint: function(){
+
+      document.getElementById("hint").style.display = 'none';
+
+  },
+
+  // keypress handlers for moving photos and hiding hint
   keyManipulateLightbox: function(e) {
 
     e = e || window.event;
 
+    lightbox.hideHint();
+
     if (e.which === 39) {
       lightbox.placeUpcomingPhoto("right");
-      // if a user presses an arrow key, hide hint
-      document.getElementById("hint").style.display = 'none';
     } else if (e.which === 37) {
       lightbox.placeUpcomingPhoto("left");
-      document.getElementById("hint").style.display = 'none';
     } else if (e.which === 27) {
       lightbox.closeLightbox();
     }
   },
 
-  // calculate container width based on number of possible squares
-  sizeContainer: function() {
-
-    var potentialSquares, measuredWidth;
-
-    potentialSquares = Math.floor((window.innerWidth - 20) / 144); // 100 width + 20 padding + 20 margin + 4 border
-    measuredWidth = (potentialSquares * 144);
-    document.getElementById("container").style.width = measuredWidth + "px";
-    window.addEventListener("resize", lightbox.sizeContainer);
-
-  },
-
   // poor man's ux hinting for keypress events
   setHintTimeout: function() {
 
-    setTimeout(function() {
-      document.getElementById("hint").style.display = 'none';
-    }, 3000);
+    setTimeout(function() { lightbox.hideHint(); }, 3000);
 
   },
 
@@ -169,21 +143,16 @@ var lightbox = {
 
     if (elem.target !== elem.currentTarget) {
 
-      clickedItem = elem.target.classList[0];
-      if (clickedItem === 'next' || clickedItem === 'prev') {
-        lightbox.placeUpcomingPhoto(elem);
-      } else if (clickedItem === 'close') {
+      // account for clicking on arrow div or arrow span
+      clickedItem = elem.target.id || elem.target.className;
+
+      if (clickedItem === "right" || clickedItem === "left") {
+        lightbox.placeUpcomingPhoto(clickedItem);
+      } else if (clickedItem === "close") {
         lightbox.closeLightbox();
       }
     }
     elem.stopPropagation();
-
-  },
-
-  // remove class that makes lightbox visible
-  closeLightbox: function() {
-
-    document.getElementById("lightbox").classList.remove('open');
 
   },
 
@@ -205,7 +174,6 @@ var lightbox = {
   },
 
   // poor man's photo cache to reduce load time for next and previous images  
-  // I considered setInterval to preload all but past a certain number of images that would become too heavy
   preloadImage: function(elem) {
 
     var nextphotoID, imgOfphotoID;
@@ -216,34 +184,29 @@ var lightbox = {
 
   },
 
-  // depending on the element clicked or key pressed advance to the next photo
+  // advance to the next photo
   placeUpcomingPhoto: function(elem) {
 
-    var photoID, elemOfphotoID, imgOfphotoID, newPhotoHTML;
+    var newPID, newestPhotoID, newPhotoSrc, newPhotoHTML;
 
-    if (typeof elem === 'string') { // keydown event doesn't transfer mouseclick object
-      photoID = document.getElementById(elem).dataset.pid;
-    } else if (elem.target.dataset.pid) { // clicking on div
-      photoID = elem.target.dataset.pid;
-    } else { // clicking on arrow span within div
-      photoID = elem.target.parentNode.dataset.pid;
-    }
+    // if elem is a next/prev string, select arrow element, else use elem passed through first click
+    newPID = (typeof elem === "string") ? document.getElementById(elem).dataset.pid : elem.target.dataset.pid;
 
-    elemOfphotoID = document.querySelectorAll("[data-pid='" + photoID + "']");
-    imgOfphotoID = elemOfphotoID[0].dataset.img;
+    newestPhotoID = document.querySelectorAll("[data-pid='" + newPID + "']");
+    newPhotoSrc = newestPhotoID[0].dataset.img;
 
-    newPhotoHTML = "<div class='lightboxphotoelem' data-pid='" + photoID + "' ";
-    newPhotoHTML += "style='background-image:url(" + imgOfphotoID + ")' ></div>";
+    newPhotoHTML = "<div class='lightboxphotoelem' data-pid='" + newPID + "' ";
+    newPhotoHTML += "style='background-image:url(" + newPhotoSrc + ")' ></div>";
 
     document.getElementById('lightboxphotoholder').insertAdjacentHTML('afterbegin', newPhotoHTML);
-    document.getElementById("lightboxcaption").innerHTML = elemOfphotoID[0].title;
+    document.getElementById("lightboxcaption").innerHTML = newestPhotoID[0].title;
 
-    // remove second element
+    // remove second photo element on a delay 
     window.setTimeout(function() {
       document.getElementsByClassName("lightboxphotoelem")[1].outerHTML = '';
     }, 100);
 
-    lightbox.setupArrowIDs(photoID);
+    lightbox.setupArrowIDs(newPID);
 
   }
 
